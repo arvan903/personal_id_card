00f6754c3432c47b1104121fabd25d0b1712bb14
arvan903 <arvan903@hotmail.com>
2022-03-15 01:15:04 +0330

    Social Media links are imported , Avatar pic linked to Github

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const String github = "https://github.com/arvan903";
+const String facebook = "https://www.facebook.com/rashid.khani.33";
+const String twitter = "https://twitter.com/rashid_khani";
+const String whatsapp = "https://wa.me/+989306056390";
+const String linkedin = "https://www.linkedin.com/in/rashid-khani-038562123";
+
 
 void main() {
   runApp(
@@ -8,6 +16,26 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  void fbURL() async {
+    if (!await launch(facebook)) throw 'Could not launch $facebook';
+  }
+
+  void waURL() async {
+    if (!await launch(whatsapp)) throw 'Could not launch $whatsapp';
+  }
+
+  void twURL() async {
+    if (!await launch(twitter)) throw 'Could not launch $twitter';
+  }
+
+  void liURL() async {
+    if (!await launch(linkedin)) throw 'Could not launch $linkedin';
+  }
+
+  void giURL() async {
+    if (!await launch(github)) throw 'Could not launch $github';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +59,12 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const CircleAvatar(
-                  backgroundImage: AssetImage('images/rashid.jpg'),
-                  radius: 50,
+                TextButton(
+                  onPressed: giURL,
+                  child: const CircleAvatar(
+                    backgroundImage: AssetImage('images/rashid.jpg'),
+                    radius: 50,
+                  ),
                 ),
                 const Text(
                   'Rashid Khani',
@@ -114,19 +145,31 @@ class MyApp extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset('images/Facebook.png'),
+                    TextButton(
+                      onPressed: fbURL,
+                      child: Image.asset('images/Facebook.png'),
+                    ),
                     const SizedBox(
                       width: 10,
                     ),
-                    Image.asset('images/Whatsapp.png'),
+                    TextButton(
+                      onPressed: waURL,
+                      child: Image.asset('images/Whatsapp.png'),
+                    ),
                     const SizedBox(
                       width: 10,
                     ),
-                    Image.asset('images/twitter.png'),
+                    TextButton(
+                      onPressed: twURL,
+                      child: Image.asset('images/twitter.png'),
+                    ),
                     const SizedBox(
                       width: 10,
                     ),
-                    Image.asset('images/Linkedin.png')
+                    TextButton(
+                      onPressed: liURL,
+                      child: Image.asset('images/Linkedin.png'),
+                    )
                   ],
                 )
               ],
