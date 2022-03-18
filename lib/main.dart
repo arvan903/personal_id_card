e8ee61ef1b3db07622fe615e6d67d5928311b970
arvan903 <arvan903@hotmail.com>
2022-03-18 22:41:37 +0330

    UI Improved , Fix some crashes , now by clicking on email address or phone number you can call or send email.

--- lib/main.dart
+++ lib/main.dart
@@ -65,172 +65,196 @@ class MyApp extends StatelessWidget {
             ),
           ),
           backgroundColor: Colors.orange,
-          body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: giURL,
-                  child: const CircleAvatar(
-                    backgroundImage: AssetImage('images/rashid.jpg'),
-                    radius: 50,
-                  ),
-                ),
-                Text(
-                  'Rashid Khani',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
-                    color: Colors.blue.shade900,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'Flutter Developer',
-                  style: TextStyle(
-                      fontFamily: 'SourceSanPro',
-                      fontSize: 20,
-                      color: Colors.teal.shade900,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  child: Divider(
-                    color: Colors.teal.shade900,
-                  ),
-                  width: 150,
-                  height: 20,
-                ),
-                Card(
-                  color: Colors.blue.shade50,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: ListTile(
-                      leading: const Icon(
-                        Icons.phone,
-                        color: Colors.teal,
+          body: Center(
+            child: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CircleAvatar(
+                      backgroundImage: AssetImage('images/rashid.jpg'),
+                      radius: 50,
+                    ),
+                    Text(
+                      'Rashid Khani',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 40,
+                        color: Colors.blue.shade900,
+                        fontWeight: FontWeight.bold,
                       ),
-                      title: Text(
-                        '+98 930 605 6390',
-                        style: TextStyle(
-                            fontFamily: 'SourceSansPro',
-                            color: Colors.teal.shade900,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold),
-                      )),
-                ),
-                Card(
-                  color: Colors.blue.shade50,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: ListTile(
-                      leading: const Icon(Icons.email, color: Colors.teal),
-                      title: Text(
-                        'Arvan903@hotmail.com',
-                        style: TextStyle(
-                            fontFamily: 'SourceSansPro',
-                            color: Colors.teal.shade900,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold),
-                      )),
-                ),
-                SizedBox(
-                  child: Divider(
-                    color: Colors.teal.shade900,
-                  ),
-                  width: 150,
-                  height: 20,
-                ),
-                Text(
-                  'Social Media',
-                  style: TextStyle(
-                      fontFamily: 'SourceSanPro',
-                      fontSize: 20,
-                      color: Colors.teal.shade900,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold),
-                ),
-                Column(
-                  children: const [
+                    ),
+                    Text(
+                      'Flutter Developer',
+                      style: TextStyle(
+                          fontFamily: 'SourceSanPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                          letterSpacing: 2.5,
+                          fontWeight: FontWeight.bold),
+                    ),
                     SizedBox(
+                      child: Divider(
+                        color: Colors.teal.shade900,
+                      ),
+                      width: 150,
                       height: 20,
+                    ),
+                    Card(
+                      color: Colors.blue.shade50,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 25),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          launch('tel:+989306056390');
+                        },
+                        child: ListTile(
+                            leading: const Icon(
+                              Icons.phone,
+                              color: Colors.teal,
+                            ),
+                            title: Text(
+                              '+98 930 605 6390',
+                              style: TextStyle(
+                                  fontFamily: 'SourceSansPro',
+                                  color: Colors.teal.shade900,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                    ),
+                    Card(
+                      color: Colors.blue.shade50,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 25),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          launch(
+                              'mailto:arvan903@hotmail.com?subject=Contacting you from ID card app title&body=Hello there');
+                        },
+                        child: ListTile(
+                          leading: const Icon(Icons.email, color: Colors.teal),
+                          title: Text(
+                            'Arvan903@hotmail.com',
+                            style: TextStyle(
+                                fontFamily: 'SourceSansPro',
+                                color: Colors.teal.shade900,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      child: Divider(
+                        color: Colors.teal.shade900,
+                      ),
+                      width: 150,
+                      height: 20,
+                    ),
+                    Text(
+                      'Social Media',
+                      style: TextStyle(
+                          fontFamily: 'SourceSanPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                          letterSpacing: 2.5,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Column(
+                      children: const [
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange.shade600),
+                            onPressed: fbURL,
+                            child: Image.asset('images/Facebook.png'),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange.shade600),
+                            onPressed: waURL,
+                            child: Image.asset('images/Whatsapp.png'),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange.shade600),
+                            onPressed: twURL,
+                            child: Image.asset('images/twitter.png'),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange.shade600),
+                            onPressed: liURL,
+                            child: Image.asset('images/Linkedin.png'),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange.shade600),
+                            onPressed: liURL,
+                            child: Image.asset('images/Telegram.png'),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange.shade600),
+                            onPressed: giURL,
+                            child: Image.asset('images/Github.png'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: const [
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange),
+                            onPressed: () {
+                              cvURL();
+                            },
+                            child: Text(
+                              'Click To View Full CV',
+                              style: TextStyle(
+                                  fontFamily: 'SourceSanPro',
+                                  letterSpacing: 1,
+                                  color: Colors.teal.shade900,
+                                  fontSize: 25),
+                            ))
+                      ],
                     ),
                   ],
                 ),
-                Flexible(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orange.shade600),
-                        onPressed: fbURL,
-                        child: Image.asset('images/Facebook.png'),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      TextButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orange.shade600),
-                        onPressed: waURL,
-                        child: Image.asset('images/Whatsapp.png'),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      TextButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orange.shade600),
-                        onPressed: twURL,
-                        child: Image.asset('images/twitter.png'),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      TextButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orange.shade600),
-                        onPressed: liURL,
-                        child: Image.asset('images/Linkedin.png'),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      TextButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orange.shade600),
-                        onPressed: liURL,
-                        child: Image.asset('images/Telegram.png'),
-                      )
-                    ],
-                  ),
-                ),
-                Column(
-                  children: const [
-                    SizedBox(height: 20),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(primary: Colors.orange),
-                        onPressed: () {
-                          cvURL();
-                        },
-                        child: Text(
-                          'Click To View Full CV',
-                          style: TextStyle(
-                              fontFamily: 'SourceSanPro',
-                              letterSpacing: 1,
-                              color: Colors.teal.shade900,
-                              fontSize: 25),
-                        ))
-                  ],
-                )
-              ],
+              ),
             ),
           ),
         ));
